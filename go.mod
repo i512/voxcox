4c004a11115c985b7417d76cde6f4c0e13a4a78a
Valerii_Fomin <Valerii_Fomin@epam.com>
2020-01-31 16:16:51 +0300

    some worked test code and fix .gitignore

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/NIHERASE/voxcox
 go 1.12
 
 require (
+	github.com/go-audio/audio v1.0.0
 	github.com/go-audio/wav v1.0.0
 	github.com/jfreymuth/pulse v0.0.0-20200107133239-fe42f62ea140
 	github.com/karalabe/xgo v0.0.0-20191115072854-c5ccff8648a7 // indirect
--- mixer/mixer_test.go
+++ mixer/mixer_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/go-audio/audio"
+	"github.com/go-audio/wav"
 )
 
 func TestOkCase(t *testing.T) {
@@ -20,13 +21,64 @@ func TestOkCase(t *testing.T) {
 	}
 }
 
-func TestMixOneFile(t *testing.T) {
-	file, err := os.Open("../samples/file_example_WAV_1MG.wav")
+func TestMixTwoFile(t *testing.T) {
+	voice1, err1 := os.Open("../samples/Spanish vocal phrase dollars-.wav")
+	defer voice1.Close()
 
-	if err != nil {
-		t.Errorf("test file not exists")
+	voice2, err2 := os.Open("../samples/Spanish vocal phrase.wav")
+	defer voice2.Close()
+
+	if err1 != nil || err2 != nil {
+		t.Errorf("file not exist's")
 	}
 
-	defer file.Close()
+	decoder1 := wav.NewDecoder(voice1)
+	decoder2 := wav.NewDecoder(voice2)
+	buffer1, err1 := decoder1.FullPCMBuffer()
+	buffer2, err2 := decoder2.FullPCMBuffer()
 
+	if err1 != nil || err2 != nil {
+		t.Errorf("i can't extract buffer from file")
+	}
+
+	// maxSoundLen := 0
+	bufferIndex := 0
+	if len(buffer1.Data) > len(buffer2.Data) {
+		// maxSoundLen = len(buffer1.Data)
+	} else {
+		// maxSoundLen = len(buffer2.Data)
+		bufferIndex = 1
+	}
+
+	if bufferIndex == 0 {
+		simpleCompareBuffer(buffer1, buffer2)
+		simpleSaveToFile(buffer1, t)
+	} else {
+		simpleCompareBuffer(buffer2, buffer1)
+		simpleSaveToFile(buffer2, t)
+	}
+
+}
+func simpleCompareBuffer(voice1, voice2 *audio.IntBuffer) {
+	for i := 0; i < len(voice2.Data); i++ {
+		voice1.Data[i] += voice2.Data[i]
+	}
+
+}
+func simpleSaveToFile(buffer *audio.IntBuffer, t *testing.T) {
+	output, err := os.OpenFile("result.wav", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		t.Errorf("i can't save result file")
+	}
+	defer output.Close()
+
+	encoder := wav.NewEncoder(
+		output,
+		buffer.PCMFormat().SampleRate,
+		buffer.SourceBitDepth,
+		buffer.PCMFormat().NumChannels,
+		1,
+	)
+	defer encoder.Close()
+	encoder.Write(buffer)
 }
